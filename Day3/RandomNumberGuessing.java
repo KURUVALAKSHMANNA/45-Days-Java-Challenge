d0fa7b021cc828db9a0074585ff503f383d3db91
Lakshmanna Kuruva <kuruvalakshmanna4154@gmail.com>
2026-05-01 22:17:17 +0530

    Update RandomNumberGuessing.java

--- Day3/RandomNumberGuessing.java
+++ Day3/RandomNumberGuessing.java
@@ -1,8 +1,4 @@
 package Day3;
-<<<<<<< HEAD
-=======
-
->>>>>>> 079e877 (Added Projects to 45 Days Java Challenge)
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,12 +7,11 @@ public class RandomNumberGuessing {
 		
 		Random random = new Random();
 		int randomNumber = random.nextInt(100) + 1;
-<<<<<<< HEAD
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("🎮 Welcome to number Guessing Game!");
 		System.out.println("Guess a number between 1 to 100\n");
 		int tryCount = 0;
-=======
 		
 		Scanner scanner = new Scanner(System.in);
 		
@@ -25,25 +20,21 @@ public class RandomNumberGuessing {
 		
 		int tryCount = 0;
 		
->>>>>>> 079e877 (Added Projects to 45 Days Java Challenge)
 		while(true) {
 			System.out.println("Enter guessing number : ");
 			int userGuessing = scanner.nextInt();
 			tryCount++;
-<<<<<<< HEAD
+
 			if(randomNumber == userGuessing) {
 				System.out.println("\n-------Your Results is-------\n");
 				System.out.println("🎉Congrats! Your Guessing is Correct! You win!");
 				System.out.println("Attempts : " + tryCount);
-
-=======
 			
 			if(randomNumber == userGuessing) {
 				System.out.println("\n\n-------Your Results is-------\n");
 				System.out.println("🎉Congrats! Your Guessing is Correct! You win!");
 				System.out.println("Attemps : " + tryCount);
 				
->>>>>>> 079e877 (Added Projects to 45 Days Java Challenge)
 				int score = 10 - tryCount;
 				if(score < 0) {
 					System.out.println("Your score is : " + score + "/10");
@@ -61,10 +52,7 @@ public class RandomNumberGuessing {
 				System.out.println("Please Try again!");
 			}
 		}
-<<<<<<< HEAD
-=======
-	
->>>>>>> 079e877 (Added Projects to 45 Days Java Challenge)
+
 		scanner.close();
 	}
 }
